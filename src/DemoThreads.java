20ad7f4fa0bfe7ae533cc5a5b4286e0b0eb7aefb
Armando Salazar <armandosalazar351@gmail.com>
2023-02-09 09:09:51 -0600

    Initial commit

---
+++ src/DemoThreads.java
@@ -0,0 +1,18 @@
+public class DemoThreads extends Thread {
+    @Override
+    public void run() {
+        super.run();
+        while (true) {
+            try {
+                System.out.println("Hola");
+                sleep(1000);
+            } catch (Exception e) {
+
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new DemoThreads().start();
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.EventListener;
+
+public class Main {
+    public static void main(String[] args) {
+        new Window().setVisible(true);
+    }
+}
+
+class Window extends JFrame {
+    private JButton button1 = new JButton("Button 1");
+    private JButton button2 = new JButton("Button 2");
+
+    Window() {
+        setTitle("Ejemplo 1");
+        setSize(500, 300);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        add(new Panel());
+        addEvents(this);
+    }
+
+    private void addEvents(Window parent) {
+        button1.addActionListener(e -> JOptionPane.showMessageDialog(parent, "Hola"));
+        button2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(parent, "Adiós");
+            }
+        });
+    }
+
+    class Panel extends JPanel {
+        Panel() {
+            add(button1);
+            add(button2);
+        }
+    }
+}
