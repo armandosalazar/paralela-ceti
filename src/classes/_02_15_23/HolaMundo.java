41d189ca68cebefb9970fb6e7dfa0b219880cce3
Armando Salazar <armandosalazar351@gmail.com>
2023-02-19 11:55:07 -0600

    add Hola mundo program

---
+++ src/classes/_02_15_23/HolaMundo.java
@@ -0,0 +1,12 @@
+package classes._02_15_23;
+
+/**
+ * Crear programa que imprima Hola Mundo
+ * Compilar y ejecutar desde consola (javac ClassName.java && java ClassName)
+ */
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+}
