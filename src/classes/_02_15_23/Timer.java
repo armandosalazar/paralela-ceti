88b9bbf3f3d500878b0bdfc1a821e33a04307f0f
Armando Salazar <armandosalazar351@gmail.com>
2023-02-21 21:17:14 -0600

    add timer

--- src/classes/_02_15_23/Timer.java
+++ src/classes/_02_15_23/Timer.java
@@ -1,4 +1,31 @@
 package classes._02_15_23;
 
-public class Timer {
+import java.io.IOException;
+
+public class Timer extends Thread {
+    private int seconds;
+    private int temp;
+
+    @Override
+    public void run() {
+        super.run();
+        while (seconds != 120000) {
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            seconds += 1000;
+            if (temp >= 59000) {
+                temp = 0;
+            } else {
+                temp += 1000;
+            }
+            System.out.println("00:".concat(String.format("%02d", seconds / 60000)).concat(":").concat(String.format("%02d", temp / 1000)));
+        }
+    }
+
+    public static void main(String[] args) {
+        new Timer().start();
+    }
 }
