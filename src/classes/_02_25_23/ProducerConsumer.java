6000e5f4f9c2730bf5d9558ab8334e1527b6b9c1
Armando Salazar <armandosalazar351@gmail.com>
2023-02-26 21:40:05 -0600

    add necesary classes

--- src/classes/_02_25_23/ProducerConsumer.java
+++ src/classes/_02_25_23/ProducerConsumer.java
@@ -1,4 +1,17 @@
 package classes._02_25_23;
 
+import java.util.ArrayList;
+
 public class ProducerConsumer {
+    public static void main(String[] args) {
+    }
+}
+
+class  Producer extends Thread {
+}
+
+class Consumer extends Thread {
+}
+
+class Product {
 }
