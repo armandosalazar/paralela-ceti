f7619bca0d648c9bb86d4c01441ee533be34763a
Armando Salazar <armandosalazar351@gmail.com>
2023-03-13 21:14:11 -0600

    add mutex for bridge

--- src/classes/_03_12_23/Bridge.java
+++ src/classes/_03_12_23/Bridge.java
@@ -1,4 +1,21 @@
 package classes._03_12_23;
 
+// El puente sería el recurso compartido
 public class Bridge {
+}
+
+class Mutex {
+    private boolean isLocked = false;
+
+    public synchronized void lock() throws InterruptedException {
+        while (isLocked) {
+            wait();
+        }
+        isLocked = true;
+    }
+
+    public synchronized void unlock() {
+        isLocked = false;
+        notify();
+    }
 }
