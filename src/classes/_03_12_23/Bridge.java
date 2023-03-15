400f72edda74adfe36fcf684b6d6db3cef4db908
Armando Salazar <armandosalazar351@gmail.com>
2023-03-15 00:02:50 -0600

    santaclaus works

--- src/classes/_03_12_23/Bridge.java
+++ src/classes/_03_12_23/Bridge.java
@@ -61,7 +61,7 @@ class Generator extends Thread {
 
             mutex.unlock();
             try {
-                sleep((long) (Math.random() * 1000));
+                sleep((int) (Math.random() * 1000));
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
--- src/classes/_03_12_23/SantaClaus.java
+++ src/classes/_03_12_23/SantaClaus.java
@@ -1,4 +1,139 @@
 package classes._03_12_23;
 
+import java.util.Random;
+
 public class SantaClaus {
+
+    public static void main(String[] args) {
+        Mutex mutex = new Mutex();
+        Reindeer reindeer = new Reindeer(mutex);
+        Santa santa = new Santa(mutex);
+        Elf elf = new Elf(mutex);
+        reindeer.start();
+        santa.start();
+        elf.start();
+    }
+
+    static class Mutex {
+        private boolean isLocked = false;
+
+        synchronized void lock() throws InterruptedException {
+            while (isLocked) {
+                wait();
+            }
+            isLocked = true;
+        }
+
+        synchronized void unlock() {
+            isLocked = false;
+            notifyAll();
+        }
+    }
+}
+
+class Santa extends Thread {
+
+    private final SantaClaus.Mutex mutex;
+
+    Santa(SantaClaus.Mutex mutex) {
+        this.mutex = mutex;
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        while (true) {
+            try {
+                mutex.lock();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Santa is working");
+            mutex.unlock();
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
+
+class Elf extends Thread {
+    private final SantaClaus.Mutex mutex;
+
+    Elf(SantaClaus.Mutex mutex) {
+        this.mutex = mutex;
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        while (true) {
+            try {
+                mutex.lock();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            int random = new Random().nextInt(1, 6);
+            if (random == 3) {
+                System.out.println("Santa is coming");
+                try {
+                    sleep(500);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            for (int i = 1; i <= random; i++) {
+                System.out.println("Elf " + i + " is working");
+                try {
+                    sleep(500);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            mutex.unlock();
+            try {
+                sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
+
+class Reindeer extends Thread {
+    private final SantaClaus.Mutex mutex;
+    private final int NUM_REINDEER = 9;
+
+    public Reindeer(SantaClaus.Mutex mutex) {
+        this.mutex = mutex;
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        while (true) {
+            try {
+                mutex.lock();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            for (int i = 1; i <= NUM_REINDEER; i++) {
+                System.out.println("Reindeer " + i + " is back from vacation");
+                try {
+                    sleep(500);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            mutex.unlock();
+            try {
+                sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 }
