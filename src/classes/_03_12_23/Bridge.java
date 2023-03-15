8b89be40cde7d743e975ff50a32ff93544dcae6d
Armando Salazar <armandosalazar351@gmail.com>
2023-03-14 19:20:04 -0600

    add features

--- src/classes/_03_12_23/Bridge.java
+++ src/classes/_03_12_23/Bridge.java
@@ -1,7 +1,18 @@
 package classes._03_12_23;
 
+import java.util.Random;
+
 // El puente sería el recurso compartido
 public class Bridge {
+    private static final Mutex mutex = new Mutex();
+
+    public static void main(String[] args) {
+        Generator north = new Generator("North", mutex);
+        Generator south = new Generator("South", mutex);
+        north.start();
+        south.start();
+    }
+
 }
 
 class Mutex {
@@ -16,6 +27,32 @@ class Mutex {
 
     public synchronized void unlock() {
         isLocked = false;
-        notify();
+        // notify();
+        notifyAll();
+    }
+}
+
+class Generator extends Thread {
+    private final Mutex mutex;
+
+    Generator(String name, Mutex mutex) {
+        super(name);
+        this.mutex = mutex;
+    }
+
+    @Override
+    public void run() {
+        // super.run();
+        while (true) {
+            // System.out.println(new Random().nextInt(1, 6) + this.getName());
+            for (int i = 0; i < new Random().nextInt(1, 6); i++) {
+                System.out.println("Car from " + this.getName());
+                try {
+                    sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
     }
 }
